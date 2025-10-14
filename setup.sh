5dd7da0ea58063e502bf13aeb512cb1de041c976
Shafeef Omar <shafeef901@gmail.com>
2025-10-14 15:49:01 +0200

    Use $NETWORK_INTERFACE from env variable

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
+
+# Source ROS 2 and CycloneDDS environments
 source /opt/ros/humble/setup.bash
 source /home/atari/dependencies/unitree_ros2/cyclonedds_ws/install/setup.bash
+
+# Check if NETWORK_INTERFACE is set
+if [ -z "$NETWORK_INTERFACE" ]; then
+    echo "⚠️ NETWORK_INTERFACE not set. Please export it before running this script."
+    exit 1
+fi
+
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enx3c4937046061" priority="default" multicast="default" />
-                        </Interfaces></General></Domain></CycloneDDS>'
+
+# Use the NETWORK_INTERFACE variable inside the XML
+export CYCLONEDDS_URI="<CycloneDDS><Domain><General><Interfaces>
+                            <NetworkInterface name=\"$NETWORK_INTERFACE\" priority=\"default\" multicast=\"default\" />
+                        </Interfaces></General></Domain></CycloneDDS>"
+
 export ROS_DOMAIN_ID=0 # Use the default domain id
+
+echo "✅ CYCLONEDDS_URI set with interface: $NETWORK_INTERFACE"
